3eb70b232aeb070a9add97f897648aa7c784da32
Ian Main <73726039+Immain@users.noreply.github.com>
2022-05-01 15:39:42 -0700

    Update lights.py

--- lights.py
+++ lights.py
@@ -1,6 +1,7 @@
 import asyncio
 
 from pywizlight import wizlight, PilotBuilder, discovery
+from lifxlan import LifxLAN, Light
 
 
 async def main():
@@ -11,10 +12,21 @@ async def main():
     for bulb in bulbs:
         print(bulb.__dict__)
 
+    light2 = wizlight("x.x.x.x")
     light = wizlight("x.x.x.x")
 
     await light.turn_on(PilotBuilder(rgb=(239, 0, 255)))
+    await light2.turn_on(PilotBuilder(rgb=(239, 0, 255)))
 
-
+    await light.turn_off()
+    await light2.turn_off()
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
+
+lifxlan = LifxLAN()
+
+livingroom = Light("00:00:00:00:00:00", "x.x.x.x")
+livingroom.set_power('on', rapid=True)
+livingroom.set_brightness('13000', rapid=True)
+
+livingroom.set_power('off', rapid=True)
